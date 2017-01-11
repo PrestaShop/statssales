26240d802ea7f317a41c91da0e7759800669ee30
antoin_m <michel.br.antoine@gmail.com>
2017-01-11 10:30:17 +0100

    Update max version compliancy

--- statssales.php
+++ statssales.php
@@ -47,7 +47,7 @@ class StatsSales extends ModuleGraph
 
 		$this->displayName = $this->l('Sales and orders');
 		$this->description = $this->l('Adds graphics presenting the evolution of sales and orders to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7.0.99');
 	}
 
 	public function install()
